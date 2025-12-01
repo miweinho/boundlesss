fb9c5cba0e4a4ac0989ec78a612159578fc89a2a
1230679 <1230679@isep.ipp.pt>
2025-12-02 00:06:31 +0100

    intro video uploaded and functional

--- Assets/Game/Scripts/UI/IntroController.cs
+++ Assets/Game/Scripts/UI/IntroController.cs
@@ -10,41 +10,34 @@ public class IntroController : MonoBehaviour
 
     void Start()
     {
-        // Garante que o botão começa desativado
         if (startButton != null)
             startButton.SetActive(false);
 
-        // Configura eventos do vídeo
         if (videoPlayer != null)
         {
             videoPlayer.loopPointReached += OnVideoEnd;
             videoPlayer.Prepare();
             videoPlayer.prepareCompleted += OnVideoPrepared;
-
-            Debug.Log("🎬 A preparar o vídeo de introdução...");
         }
         else
         {
-            Debug.LogError("❌ Nenhum VideoPlayer atribuído ao IntroController!");
+            Debug.LogError("No video attributed");
         }
     }
 
     private void OnVideoPrepared(VideoPlayer vp)
     {
-        Debug.Log("▶️ Vídeo preparado — a iniciar reprodução.");
         vp.Play();
     }
 
     private void OnVideoEnd(VideoPlayer vp)
     {
-        Debug.Log("🏁 Vídeo terminou — a mostrar botão START.");
         if (startButton != null)
             startButton.SetActive(true);
     }
 
     public void StartGame()
     {
-        Debug.Log("🎮 A iniciar o jogo...");
         SceneManager.LoadScene("FightScene");
     }
 }
